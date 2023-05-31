5e54ade81e113b2b9cf52df0f702ef2d2e57afef
TzuWei <ap9940506@gmail.com>
2023-05-31 15:13:33 +0800

    feat: determine base url by env

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const fetcher = axios.create({
-  baseURL: "http://localhost:8000",
-  // baseURL: "https://meter.hackyour.pw/",
+  baseURL:
+    process.env.NODE_ENV === "production" ? "https://meter.hackyour.pw/" : "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
