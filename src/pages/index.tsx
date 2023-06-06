7db7e1aea0256324e23d2e6611e2312eec78fba3
TzuWei <ap9940506@gmail.com>
2023-06-07 03:30:23 +0800

    fix: temporarily comment out earthquake for successful preview deploy'

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { type NextPage } from "next";
-import Earthquake from "~/components/dashboard/earthquake";
+// import Earthquake from "~/components/dashboard/earthquake";
 import Electricity from "~/components/dashboard/electricity";
 import Overview from "~/components/dashboard/overview";
 import Reservoir from "~/components/dashboard/reservoir";
@@ -13,13 +13,13 @@ import { type EletricityResource, type WaterResource } from "~/lib/resource";
 const Dashboard: NextPage = () => {
   const { data: powerData } = useSWR<{ data: EletricityResource[] }>("/report/power", fetcher);
   const { data: waterData } = useSWR<{ data: WaterResource[] }>("/report/dam", fetcher);
-  const { data: quakeData } = useSWR("/report/eq", fetcher);
+  // const { data: quakeData } = useSWR("/report/eq", fetcher);
   return (
     <div className="flex w-full flex-col gap-6 md:px-6">
       <Overview />
       <Electricity data={powerData?.data.at(-1)} />
       <Reservoir data={waterData?.data} />
-      <Earthquake data={quakeData} />
+      {/* <Earthquake data={quakeData} /> */}
       {/* <Separator className="my-1" />
       <Weather />
       <Separator className="my-1" /> */}
